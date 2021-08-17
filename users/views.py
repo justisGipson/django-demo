4b71421c72abac0a5a95afe4bcb2a29c8933f006
Justis <justis710@gmail.com>
2021-08-17 14:41:22 -0400

    add register new users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from .forms import UserForm
 
 
 def log_out(req):
     logout(req)
     return redirect("listings:index")
+
+
+def register(req):
+    if req.method != "POST":
+        form = UserForm()
+    else:
+        form = UserForm(data=req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users:login")
+
+    context = {"form": form}
+    return render(req, "registration/register.html", context)
